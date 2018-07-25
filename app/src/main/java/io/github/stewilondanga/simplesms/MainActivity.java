ae6f5ba732e383a17a5ba66cecb2f3ea03510205
Stewart ilondanga <stilondanga@gmail.com>
2018-07-25 10:18:09 +0300

    add app/src/main/java/io/github/stewilondanga/simplesms/MainActivity.java

--- app/src/main/java/io/github/stewilondanga/simplesms/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/simplesms/MainActivity.java
@@ -1,5 +1,10 @@
 package io.github.stewilondanga.simplesms;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+            }
+        } else {
+            // do nothing
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
